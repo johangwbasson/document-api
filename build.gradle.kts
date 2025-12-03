e4dbd420c69cfe72156569f9016043a05c19b0d2
Johan Basson <johangwbasson@gmail.com>
2025-12-03 19:18:27 +0200

    Add additional dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,29 @@ dependencies {
     implementation(libs.http4k.ops.opentelemetry)
     implementation(libs.http4k.ops.resilience4j)
 
+    // Logging
+    implementation(libs.logging.kotlin)
+    implementation(libs.logging.logback)
+    implementation(libs.logging.slf4j)
+
+    // Arrow-kt
+    implementation(libs.arrow.core)
+    implementation(libs.arrow.fx.coroutines)
+
+    // BCrypt
+    implementation(libs.bcrypt.core)
+
+    // JWT
+    implementation(libs.jwt.core)
+
+    // Test dependencies\
     testImplementation(kotlin("test"))
+    testImplementation(libs.restAssured.core)
+    testImplementation(libs.kotest.runner.junit5)
+    testImplementation(libs.kotest.assertions.core)
+    testImplementation(libs.kotest.property)
+    testImplementation(libs.kotest.assertions.arrow)
+    testImplementation(libs.mockk.core)
 }
 
 tasks.test {
